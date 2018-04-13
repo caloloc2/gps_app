a827a1cb722b35827a2ceab914fb8c030d4b6145
caloloc2 <calolomino@gmail.com>
2018-04-13 16:44:37 -0500

    actualiza

--- vehiculo/index.php
+++ vehiculo/index.php
@@ -7,7 +7,13 @@ if ((isset($datos['vehiculo']))&&(isset($datos['placa']))&&(isset($datos['color'
 
 	$ingreso = Meta::Nuevo_Vehiculo($datos['vehiculo'], $datos['placa'], $datos['color']);
 
-	$respuesta['estado'] = "Se ingreso correctamente la informacion";
+	$consulta = Meta::Consulta_Unico("SELECT * FROM vehiculos ORDER BY id_vehiculo DESC LIMIT 1");
+	if ($consulta['id_vehiculo']!=''){
+		$respuesta['estado'] = "Se ingreso correctamente la informacion. Su ID de vehiculo es ".$consulta['id_vehiculo'];
+	}else{
+		$respuesta['error'] = "Hubo un error al registrar la informacion";
+	}
+	
 }else{
 	$respuesta['error'] = "Debe incluir el vehiculo (nombre o marca), placa y color obligatoriamente.";
 }
